efd71ee1cafb5bc17c2a95d37e3c14a97135e820
Aim <47038179+AliMouradd@users.noreply.github.com>
2020-10-17 20:48:36 -0700

    Added Final Case Consideration
    -also added file write

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         Scanner reader = new Scanner(new File("input.txt"));
         int magnetCount = reader.nextInt();
-        System.out.println(magnetCount);
+//        System.out.println(magnetCount);
 
         String lineOne = reader.next();
         lineOne += reader.next();
 
-        System.out.println(lineOne.length() + "\n" + lineOne);
+//        System.out.println(lineOne.length() + "\n" + lineOne);
 
         char [][] charArr = new char [lineOne.length()/2][2];
 
@@ -22,53 +19,112 @@ public class Main {
             for(int j = 0; j<charArr[i].length-1; j+=2){
                 charArr[i][j] = lineOne.charAt(i);
                 charArr[i][j+1] = lineOne.charAt(i+(lineOne.length()/2));
-                System.out.println("Values at arr["+i+"]["+j+"] ["+i+"]["+(j+1)+"] is "+charArr[i][j] + " " + charArr[i][j+1]);
+//                System.out.println("Values at arr["+i+"]["+j+"] ["+i+"]["+(j+1)+"] is "+charArr[i][j] + " " + charArr[i][j+1]);
             }
         }
 
-        System.out.println(Arrays.deepToString(charArr));
+        // System.out.println(Arrays.deepToString(charArr));
 
         String location = "";
-        while(magnetCount != 0){
-            for(int i = 0; i < charArr.length; i++){
-                for(int j = 0; j < charArr[i].length-1; j+=2){
-                    // Checks space for [+ ,*] then fills * with -
-                    if(charArr[i][j] == '+' && charArr[i][j+1] == '*'){
-                        charArr[i][j+1] = '-';
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + j + " "+ i + " " + (j+1) + "\n");
-                        magnetCount--;
+        for(int i = 0; i < charArr.length; i++){
+            if(magnetCount == 0){
+                break;
+            }
+            //System.out.println("Outerloop not **");
+            for(int j = 0; j < charArr[i].length-1; j+=2){
+
+                if(magnetCount == 0){
+                    break;
+                }
+                //System.out.println("Inner loop not **");
+                // Checks space for [+ ,*] then fills * with -
+                if(charArr[i][j] == '+' && charArr[i][j+1] == '*'){
+                    charArr[i][j+1] = '-';
+                    //keep track of location of full magnet using string
+                    location += (i +" " + j + " "+ i + " " + (j+1) +"\n");
+                    magnetCount--;
+
                     // Checks Space for [*, +] and fills * with -
-                    }else if(charArr[i][j] == '*' && charArr[i][j+1] == '+'){
-                        charArr[i][j] = '-';
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
-                        magnetCount--;
+                }else if(charArr[i][j] == '*' && charArr[i][j+1] == '+'){
+                    charArr[i][j] = '-';
+                    //keep track of location of full magnet using string
+                    location += (i +" " + (j+1) + " "+ i + " " + j+"\n" );
+                    magnetCount--;
+
                     //checks space for [-,*] and fills * with +
-                    }else if(charArr[i][j] == '-' && charArr[i][j+1] == '*'){
-                        charArr[i][j+1] = '+';
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
-                        magnetCount--;
+                }else if(charArr[i][j] == '-' && charArr[i][j+1] == '*'){
+                    charArr[i][j+1] = '+';
+                    //keep track of location of full magnet using string
+                    location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
+                    magnetCount--;
+
                     //checks space for [*,-] and fills * with +
-                    }else if(charArr[i][j] == '*' && charArr[i][j+1] == '-'){
-                        charArr[i][j] = '+';
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + j + " "+ i + " " + (j+1)+ "\n");
-                        magnetCount--;
+                }else if(charArr[i][j] == '*' && charArr[i][j+1] == '-'){
+                    charArr[i][j] = '+';
+                    //keep track of location of full magnet using string
+                    location += (i +" " + j + " "+ i + " " + (j+1)+ "\n");
+                    magnetCount--;
+
                     //checks space for [+,-] and keeps track of magnet location
-                    }else if(charArr[i][j] == '+' && charArr[i][j+1] == '-'){
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + j + " "+ i + " " + (j+1)+ "\n");
-                        magnetCount--;
+                }else if(charArr[i][j] == '+' && charArr[i][j+1] == '-'){
+                    //keep track of location of full magnet using string
+                    location += (i +" " + j + " "+ i + " " + (j+1)+ "\n");
+                    magnetCount--;
+
                     //checks space for [-,+] and fills * with +
-                    }else if(charArr[i][j] == '-' && charArr[i][j+1] == '+'){
-                        //keep track of location of full magnet using string 
-                        location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
-                        magnetCount--;
+                }else if(charArr[i][j] == '-' && charArr[i][j+1] == '+'){
+                    //keep track of location of full magnet using string
+                    location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
+                    magnetCount--;
+
+                }
+            }
+        }
+
+        if(magnetCount != 0){
+            for(int i = 0; i < charArr.length; i++){
+                if(magnetCount == 0){
+                    break;
+                }
+                //System.out.println("Outerloop **");
+                for(int j = 0; j < charArr[i].length-1; j+=2){
+                    if(magnetCount == 0){
+                        break;
+                    }
+                    //System.out.println("Inner loop **");
+                    if(charArr[i][j] == '*' && charArr[i][j+1] == '*'){
+                        if(i == charArr.length-1){
+                            if(charArr[i-1][j] =='+'){
+                                charArr[i][j] = '-';
+                                charArr[i][j+1] = '+';
+                                location += (i +" " + (j+1) + " "+ i + " " + j+ "\n");
+                                magnetCount --;
+                            }else{
+                                charArr[i][j] = '+';
+                                charArr[i][j+1] = '-';
+                                location += (i +" " + j + " "+ i + " " + (j+1)+ "\n");
+                                magnetCount --;
+                            }
+                        }else {
+                            if (charArr[i + 1][j] == '+') {
+                                charArr[i][j] = '-';
+                                charArr[i][j + 1] = '+';
+                                location += (i + " " + (j + 1) + " " + i + " " + j +  "\n");
+                                magnetCount--;
+                            } else {
+                                charArr[i][j] = '+';
+                                charArr[i][j + 1] = '-';
+                                location += (i + " " + j + " " + i + " " + (j + 1) +  "\n");
+                                magnetCount--;
+                            }
+                        }
                     }
                 }
             }
         }
+
+        FileWriter writer = new FileWriter("output.txt");
+        writer.write(location);
+        writer.close();
     }
 }
